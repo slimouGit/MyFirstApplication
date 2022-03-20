4bbdc4dda183295651d5990222a39092ec0d48e1
Salim Oussayfi <box@slimou.net>
2022-03-20 18:52:33 +0100

    working with multiple activities and using simple navigation

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,39 +1,28 @@
 package com.example.myfirstapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // These are the global variables
-    EditText editText;
-    TextView result;
-    Button buttonSubmit;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        editText  = (EditText) findViewById(R.id.editText);
-        result = (TextView) findViewById(R.id.result);
-        buttonSubmit = (Button) findViewById(R.id.button);
-
-        buttonSubmit.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String input = editText.getText().toString();
-                result.setText(input);
-
-            }
-        });
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
